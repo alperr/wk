01f28953a013d97351a97747c35e81de9291a2ea
Alper Çınar <alper3017@gmail.com>
2019-02-23 01:18:16 +0300

    npm release

--- wk.js
+++ wk.js
@@ -18,7 +18,7 @@ const COMPONENT_BASE_PATH = "./com/";
 const CLASS_BASE_PATH = "./src/";
 const OUTPUT_PATH = "./www/dev";
 
-const VERSION = "0.2.8";
+const VERSION = "0.2.9";
 var commands =
 {
 	"init"  : init,
